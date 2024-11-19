b51e1421b0c19f8eaaf91b262c42edd6d293190a
MaverickCod <921chavanprem@gmail.com>
2024-11-19 20:49:13 +0530

    completed validating access token

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import {createCurrentUser} from "../controllers/MyUserController";
+import { createCurrentUser } from "../controllers/MyUserController";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 // /api/my/user 
-router.post("/", createCurrentUser);
+router.post("/", jwtCheck, createCurrentUser);
 
 
 export default router;
--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -7,11 +7,14 @@ type Props = {
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const navigate = useNavigate();
+
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
-    if(!domain || !clientId || !redirectUri) {
+
+    if(!domain || !clientId || !redirectUri || !audience) {
         throw new Error ("unable to initialize auth")
     }
 
@@ -25,6 +28,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         clientId={clientId} 
         authorizationParams={{
             redirect_uri:redirectUri,
+            audience,
         }}
         onRedirectCallback={onRedirectCallback}
         >
