8a3f761106dccb1f1e90490534f300dbff0be4cf
MaverickCod <921chavanprem@gmail.com>
2024-11-22 11:56:58 +0530

    now working on user profile form

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -2,19 +2,35 @@ import { body, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 
 // Middleware to handle validation errors
-const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
+const handleValidationErrors = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
 };
 
 // Exporting the validation rules as an array
 export const validateMyUserRequest = [
-    body("name").isString().notEmpty().withMessage("Name must be a string"),
-    body("AddressLane1").isString().notEmpty().withMessage("AddressLane1 must be a string"),
-    body("city").isString().notEmpty().withMessage("City must be a string"),
-    body("country").isString().notEmpty().withMessage("Country must be a string"),
-    handleValidationErrors,
+  body("name")
+    .isString()
+    .notEmpty()
+    .withMessage("Name must be a string"),
+  body("AddressLane1")
+    .isString()
+    .notEmpty()
+    .withMessage("AddressLane1 must be a string"),
+  body("city")
+    .isString()
+    .notEmpty()
+    .withMessage("City must be a string"),
+  body("country")
+    .isString()
+    .notEmpty()
+    .withMessage("Country must be a string"),
+  handleValidationErrors,
 ];
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { createCurrentUser, updateCurrentUser } from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import {validateMyUserRequest} from "../middleware/validation";
 
 const router = express.Router();
 
 // /api/my/user 
 router.post("/", jwtCheck, createCurrentUser);
-router.put("/", jwtCheck, jwtParse, updateCurrentUser)
-
+router.put(
+    "/",
+    jwtCheck,
+    jwtParse,
+    validateMyUserRequest,
+    updateCurrentUser
+  );
+  
 export default router;
